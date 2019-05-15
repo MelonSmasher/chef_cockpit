0c158a895d5589a2c8729eac6fe4575a6532ac36
Alex Markessinis <markea125@gmail.com>
2019-05-15 11:53:44 -0400

    fixed debian jessie backports url

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,57 +3,68 @@
 # Recipe:: default
 #
 
-# Enable repos on Debian and RHEL
+# Run only on supported platforms
 case node['platform']
-when 'debian'
-  case node['lsb']['codename']
-  when 'jessie'
-    apt_repository 'backports' do
-      uri 'http://deb.debian.org/debian/'
-      distribution 'jessie-backports-sloppy'
-      components ['main']
+when 'debian', 'ubuntu', 'centos', 'redhat'
+
+# Enable repos on Debian and RHEL
+  case node['platform']
+  when 'debian'
+    case node['lsb']['codename']
+    when 'jessie'
+      apt_repository 'backports-sloppy' do
+        uri 'http://archive.debian.org/debian/'
+        distribution 'jessie-backports-sloppy'
+        components ['main']
+      end
+    when 'stretch'
+      apt_repository 'backports' do
+        uri 'http://deb.debian.org/debian/'
+        distribution 'stretch-backports'
+        components ['main']
+      end
     end
-  when 'stretch'
-    apt_repository 'backports' do
-      uri 'http://deb.debian.org/debian/'
-      distribution 'stretch-backports'
-      components ['main']
+  when 'redhat'
+    rhsm_repo 'rhel-7-server-extras-rpms' do
+      action :enable
     end
   end
-when 'redhat'
-  rhsm_repo 'rhel-7-server-extras-rpms' do
-    action :enable
-  end
-end
 
 # Install, upgrade or remove the package
-case node['cockpit_install']['action']
-when 'install', 'upgrade', 'remove'
-  package 'cockpit' do
-    action node['cockpit_install']['action'].to_s.to_sym
+  case node['cockpit_install']['action']
+  when 'install', 'upgrade', 'remove'
+    package 'cockpit' do
+      action node['cockpit_install']['action'].to_s.to_sym
+    end
+  else
+    # Install a specific version
+    package 'cockpit' do
+      action :install
+      version node['cockpit_install']['action'].to_s
+    end
   end
-else
-  # Install a specific version
-  package 'cockpit' do
-    action :install
-    version node['cockpit_install']['action'].to_s
-  end
-end
 
 # Enable and start the service
-service 'cockpit.socket' do
-  action [:enable, :start]
-end
+  service 'cockpit.socket' do
+    action [:enable, :start]
+  end
 
 # Add firewall rules on CentOS and RHEL
-case node['platform']
-when 'redhat'
-  firewalld_service 'cockpit' do
-    action :add
+  case node['platform']
+  when 'redhat'
+    firewalld_service 'cockpit' do
+      action :add
+    end
+  when 'centos'
+    firewalld_service 'cockpit' do
+      action :add
+      zone 'public'
+    end
   end
-when 'centos'
-  firewalld_service 'cockpit' do
-    action :add
-    zone 'public'
+
+else
+  log 'cockpit_install' do
+    message "The platform : #{node['platform']} is not supported at this time."
+    level :warn
   end
 end
